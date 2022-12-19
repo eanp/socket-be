11efab471f49363eab72ee4f8c1cc5707c2b58b9
eanp <awifamz@gmail.com>
2022-12-19 10:18:05 +0700

    group chat

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+var moment = require('moment'); 
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, { 
     cors : {
-        origin: "http://localhost:3001"
+        origin: "http://localhost:3000"
     }
  });
 const PORT = 4000
@@ -14,12 +15,26 @@ const PORT = 4000
 io.on("connection", (socket) => {
     console.log(`user connect ${socket.id}`)
 
+    socket.on("initialRoom",({room})=>{
+        console.log(room)
+        socket.join(`room:${room}`)
+    })
+
+
     socket.on("message",(data)=>{
-        let time = new Date()
-        io.emit("messageBe",{message: data, date: time})
-        // socket.broadcast.emit("messageBe",{message: data, date: time})
+        io.to(`room:${data.group}`).emit("messageBe",{
+            sender: data.name,
+            message: data.message,
+            date:  moment().format("HH:mm")
+        })
         console.log(data)
     })
+    
+    // socket.on("message",(data)=>{
+    //     io.emit("messageBe",{message: data, date:  moment().format("HH:mm")})
+    //     // socket.broadcast.emit("messageBe",{message: data, date: time})
+    //     console.log(data)
+    // })
 
     socket.on("disconnect",()=>{
         console.log(`user disconnect ${socket.id}`)
